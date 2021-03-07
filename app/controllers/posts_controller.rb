b3b0aef52dfef88b789b8c5100d64c9bdd0022c5
kedani <75819419+kedani@users.noreply.github.com>
2021-03-07 18:35:21 +0900

    画像

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
@@ -8,5 +8,10 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :content)
   end
 end
